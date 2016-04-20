86575dda4f0db0766e516c55d2676a04cf784bfc
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2016-04-20 15:20:26 +0200

    akka simple cluster

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.4",
+  "com.typesafe.akka" %% "akka-cluster" % "2.4.4",
   "com.typesafe" % "config" % "1.3.0",
   "io.spray" %% "spray-can" % "1.3.3",
   "io.spray" %% "spray-routing" % "1.3.3",
---
+++ src/main/scala/com/boxpizza/ClusterBoxPizza.scala
@@ -0,0 +1,24 @@
+package com.boxpizza
+
+import akka.actor.{ActorSystem, Props}
+import akka.routing._
+import akka.cluster._
+import akka.cluster.routing._
+import com.typesafe.config.ConfigFactory
+import com.boxpizza.actors.WaiterActor
+import com.boxpizza.api.RestInterface
+
+object SingleBoxPizza extends App {
+  val config = ConfigFactory.parseString(s"""
+    akka.actor.provider = akka.cluster.ClusterActorRefProvider
+    akka.cluster.seed-nodes = ["akka.tcp://ClusterBoxPizza@127.0.0.1:2500"]
+    """)
+  implicit val system = ActorSystem("ClusterBoxPizza", config)
+  Cluster(system).registerOnMemberUp {
+    val roundRobinPool = RoundRobinPool(nrOfInstances = 10)
+    val clusterRoutingSettings = ClusterRouterPoolSettings(totalInstances = 10, maxInstancesPerNode = 5, allowLocalRoutees = true, useRole = None)
+    val clusterPool = ClusterRouterPool(roundRobinPool, clusterRoutingSettings)
+    val router = system.actorOf(clusterPool.props(Props[WaiterActor]))
+    system.actorOf(Props(new RestInterface(router, 8080)))
+  }
+}
